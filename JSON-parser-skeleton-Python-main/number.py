9038aa5e6467a3ebc07c5512a24031ab40e9856f
georgepauly <georgepauly1995@gmail.com>
2025-02-19 15:00:21 +0530

    testing modifying already uploaded files

--- JSON-parser-skeleton-Python-main/number.py
+++ JSON-parser-skeleton-Python-main/number.py
@@ -1,21 +1,21 @@
 import re
 
 def parse_number(input_string):
-    scientific_float_pattern = "^[-]?(([1-9][0-9]*[.][0-9]*(e|E)[-+]?[1-9][0-9]*)|([1-9][0-9]*(e|E)[-+]?[1-9][0-9]*)|(0(e|E)[-+]?[1-9][0-9]*))"
+    scientific_float_pattern = "^[-]?(([1-9]([0-9]+)?[.][0-9]+(e|E)[-+]?[0-9]+)|([1-9][0-9]*(e|E)[-+]?[0-9]+)|(0[.][0-9]+(e|E)[-+]?[0-9]+)|(0(e|E)[-+]?[0-9]+))$"
     data = re.search(scientific_float_pattern,input_string)
     if data is not None:
         remaining_string = input_string[len(data[0]):]
         final_output = (float(data[0]),remaining_string)
         return final_output
     else:
-        float_pattern = "^[-]?([1-9][0-9]*[.][0-9]+|0[.][0-9]+)"
+        float_pattern = "^[-]?([1-9][0-9]*[.][0-9]+|0[.][0-9]+)$"
         data = re.search(float_pattern, input_string)
         if data is not None:
             remaining_string = input_string[len(data[0]):]
             final_output = (float(data[0]), remaining_string)
             return final_output
         else:
-            integer_pattern = "^[-]?([1-9][0-9]*)"
+            integer_pattern = "^[-]?([1-9][0-9]*|0)$"
             data = re.search(integer_pattern,input_string)
 
             if data is not None:
@@ -49,10 +49,10 @@ for x in content_list:
         passed_test_cases_true_positive.append(x_list[0])
 #Checking the Results
 print(f"Total test case count: {len(content_list)}")
-# print(f"correctly detected numbers: {len(passed_test_cases_true_positive)}, {passed_test_cases_true_positive}")
-# print(f"correctly detected non numbers: {len(passed_test_cases_true_negative)}, {passed_test_cases_true_negative}")
+print(f"correctly detected numbers: {len(passed_test_cases_true_positive)}")
+print(f"correctly detected non numbers: {len(passed_test_cases_true_negative)}")
 print(f"wrongly detected non numbers: {len(failed_test_cases_false_negative)}, {failed_test_cases_false_negative}")
-#print(f"wrongly detected numbers: {len(failed_test_cases_false_positive)}, {failed_test_cases_false_positive}")
+print(f"wrongly detected numbers: {len(failed_test_cases_false_positive)}, {failed_test_cases_false_positive}")
 
 
 
