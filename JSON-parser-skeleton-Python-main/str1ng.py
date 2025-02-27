173b5731cd8e4805d94ed14ea8f7351f452ffc9f
georgepauly <georgepauly1995@gmail.com>
2025-02-27 09:42:02 +0530

    Refactored to combine scientific regex pattern with float and integer pattern and removed redundant else statements.

--- JSON-parser-skeleton-Python-main/str1ng.py
+++ JSON-parser-skeleton-Python-main/str1ng.py
@@ -1,27 +1,34 @@
 import re
-# def parse_string(input_string):
-#     """Parses string
-#     >>> parse_string('"abc" rest')
-#     ("abc", " rest")
-#     >>> parse_string("something else")
-#     None
-#     """
-
-pattern = r'(\\|/|"|[\b]|\f|\n|\r|\t|\uFFFF)'
-
-f = open("positive_string_tests_v2.txt", "r")
+#reading test case files
+f = open("positive_string_tests.txt", "r")
 content = f.read()
-content_list = content.split('\n')
-print(content_list[0:99])
-wrongly_matched = 0
-correctly_matched = 0
-for i in range(10000):
-    data = re.search(pattern, content_list[i])
-    if data is None:
-        correctly_matched += 1
-        print(f"{data} line no: {i+1} with string : {content_list[i]} is correctly matched")
+content_list = list(content.split('\n'))
+pattern  = '^"([^"(\\\\)\x00\x01\x08\x09\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f]*|(\\\\b)*|(\\\\n)|(\\\\t)*|(\\\\\\\\)*|(\\\")*|(\\\\/)*|(\\\\r)*|(\\\\f)*|(\\\\u[a-fA-F0-9]{4})*)"$'
+print(content_list[1510])
+#string parser function
+def string_parser(input_string):
+    data = re.search(pattern,input_string)
+    if data is not None:
+        return data[0]
+    return None
+
+#Testing the function
+print(string_parser(content_list[1510]))
+
+count_none = 0 # should be 0 for the entire positive string test case
+count_non_none = 0 # should be 0 for the entire negative string test case
+n = len(content_list)
+for i in range(1510,1511):
+    if string_parser(content_list[i]) is None:
+        count_none += 1
+        # (for negative test case) print(f"line no. {i+1} is detected correct: {content_list[i]}")
+        #print(f"line no. {i+1} is detected wrong: {string_parser(content_list[i])}, {content_list[i]}") # (for positive test case)
     else:
-        wrongly_matched += 1
-        #print(f"line no: {i + 1} with string : {content_list[i]} is wrongly matched")
-print(f"There are {correctly_matched} correctly matched test cases")
-print(f"There are {wrongly_matched} wrongly matched test cases")
+        count_non_none +=1
+        # (for negative test case) print(f"line no. {i+1} is detected wrong,: {string_parser(content_list[i])}")
+        #print(f"line no. {i+1} is detected correct (but maybe parsed wrong): {string_parser(content_list[i])}") # (for positive test case)
+
+#Checking >90K test cases for valid and invalid strings each.
+
+print(f"None Counts: {count_none}")
+print(f"Non None Counts: {count_non_none}")
