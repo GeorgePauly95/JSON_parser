964a080ae252b596842d7a4384e18bc43d90e464
georgepauly <georgepauly1995@gmail.com>
2025-03-07 09:50:01 +0530

    Array parser without Object as an element.

--- JSON-parser-skeleton-Python-main/str1ng.py
+++ JSON-parser-skeleton-Python-main/str1ng.py
@@ -1,36 +1,81 @@
 import re
+import json
+from ast import parse
+
 #reading test case files
 f = open("positive_string_tests.txt", "r")
 content = f.read()
 content_list = list(content.split('\n'))
-pattern  = '^"([^"(\\\\)\x00\x01\x08\x09\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f]*|(\\\\b)*|(\\\\n)|(\\\\t)*|(\\\\\\\\)*|(\\\")*|(\\\\/)*|(\\\\r)*|(\\\\f)*|(\\\\u[a-fA-F0-9]{4})*)"$'
-print(content_list[1510])
 
 #string parser function
-def string_parser(input_string):
+def parse_string(input_string):
+    # """Parses string
+    #
+    #     >>> parse_string('"abc" rest')
+    #     ('abc', ' rest')
+    #
+    #     >>> parse_string("something else") is None
+    #     True
+    #
+    # """
+    pattern = '^\"([^\"\\\\\x00-\x1f\x7f-\x9f]|(\\\\(b|n|t|\\\\|"|r|f))|(\\\\u[a-fA-F0-9]{4}))*\"'
     data = re.search(pattern,input_string)
     if data is not None:
-        return data[0]
+        output = (data[0])[1:-1]
+        if '\\' in output:
+            fin_output = output.encode().decode('unicode_escape')
+            return fin_output, input_string[len(data[0]):]
+        return output, input_string[len(data[0]):]
     return None
 
-#Testing the function
-print(string_parser(content_list[1510]))
+# n = len(content_list)
 
-#Checking >90K test cases for valid and invalid strings each.
-count_none = 0 # should be 0 for the entire positive string test case
-count_non_none = 0 # should be 0 for the entire negative string test case
-n = len(content_list)
-for i in range(n):
-    if string_parser(content_list[i]) is None:
-        count_none += 1
-        # (for negative test case) print(f"line no. {i+1} is detected correct: {content_list[i]}")
-        #print(f"line no. {i+1} is detected wrong: {string_parser(content_list[i])}, {content_list[i]}") # (for positive test case)
-    else:
-        count_non_none +=1
-        # (for negative test case) print(f"line no. {i+1} is detected wrong,: {string_parser(content_list[i])}")
-        #print(f"line no. {i+1} is detected correct (but maybe parsed wrong): {string_parser(content_list[i])}") # (for positive test case)
+# Testing the function against python's json library
+# Initializing correctness tracking variables
+
+# correct_count = 0
+# wrong_count = 0
+# json_lib_unicode_error_count = 0
+# unknown_json_lib_decode_error_count = 0
+# backslash_count = 0
+# for i in range(n):
+#     try:
+#         if json.loads(content_list[i]) == parse_string(content_list[i]):
+#             correct_count += 1
+#             #print("Line no:", i + 1, parse_string(content_list[i]), "equal", json.loads(content_list[i]))
+#             continue
+#         wrong_count += 1
+#         print("Line no:", i+1, parse_string(content_list[i]), "unequal", json.loads(content_list[i]), content_list[i])
+#     except json.decoder.JSONDecodeError:
+#         print(f"line no. {i+1}: invalid string")
+#     except UnicodeEncodeError:
+#         print(f"line no. {i+1}: invalid string for the json lib")
+#
+# print(f"Correct count: {correct_count}")
+# print(f"Wrong count: {wrong_count}")
+# print(f"Issues with json lib encoding: {json_lib_unicode_error_count}")
 
 
+#Testing the function against test cases
+# Checking >90K test cases for valid and invalid strings each.
 
-print(f"None Counts: {count_none}")
-print(f"Non None Counts: {count_non_none}")
+# count_none = 0 # should be 0 for the entire positive string test case
+# count_non_none = 0 # should be 0 for the entire negative string test case
+# unicode_error_count = 0
+# for i in range(n):
+#     try:
+#         if parse_string(content_list[i]) is None:
+#             count_none += 1
+#             #print("line no.", i+1, "detected correct", content_list[i])# (for negative test case)
+#             print("line no." ,i+1, "is detected wrong", parse_string(content_list[i]), content_list[i])# (for positive test case)
+#         else:
+#             count_non_none +=1
+#             #print(f"line no.", i+1, "detected wrong", parse_string(content_list[i]))# (for negative test case)
+#             print("line no." ,i+1, "is detected correct", parse_string(content_list[i]))# (for positive test case)
+#     except UnicodeEncodeError:
+#         print("unicode error on line no:", i+1)
+#         unicode_error_count += 1
+
+# print(f"None Counts: {count_none}")
+# print(f"Non None Counts: {count_non_none}")
+# print(unicode_error_count)
